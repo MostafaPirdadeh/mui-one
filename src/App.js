72f2ae80380d186e997557a36ee5064b921dae15
Mostafa Pirdadeh <116708385+MostafaPirdadeh@users.noreply.github.com>
2023-08-11 01:04:28 +0330

    step two

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/feed";
+import Rightbar from "./components/Rightbar";
+import { Box, Stack } from "@mui/material";
 
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <Box>
+      <Stack direction={"row"} spacing={2} justifyContent={"space-between"}>
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
   );
 }
 
