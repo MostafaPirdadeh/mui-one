1217238c62769e081b56acc01d7c38142d54e29e
Mostafa Pirdadeh <116708385+MostafaPirdadeh@users.noreply.github.com>
2023-08-11 23:42:38 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/feed";
 import Rightbar from "./components/Rightbar";
+import Navbaraassaaa
 import { Box, Stack } from "@mui/material";
 
 function App() {
   return (
     <Box>
+      <Navbar/>
       <Stack direction={"row"} spacing={2} justifyContent={"space-between"}>
         <Sidebar />
         <Feed />
